d0c26b4e6d4aeee91a82f748e7909bdd1f881194
stefba <st@rg-s.com>
2019-11-23 00:10:03 +0100

    Fix missing translations in index main

--- pkg/handlers/index/main.go
+++ pkg/handlers/index/main.go
@@ -37,12 +37,28 @@ func Main(s *server.Server, w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	recents := s.Recents["index"].Offset(0, 100)
+
 	err = s.ExecuteTemplate(w, "index-hold", &indexMain{
 		Head:    head,
 		Hold:    s.Trees["index"],
-		Recents: s.Recents["index"],
+		Recents: deleteEmpty(recents, lang),
 	})
 	if err != nil {
 		log.Println(err)
 	}
+}
+
+// Temporary workaround
+func deleteEmpty(entries entry.Els, lang string) entry.Els {
+	clean := entry.Els{}
+	for _, e := range entries {
+		if entry.Type(e) == "text" {
+			if e.(*entry.Text).Text[lang] == "" {
+				continue
+			}
+		}
+		clean = append(clean, e)
+	}
+	return clean
 }
