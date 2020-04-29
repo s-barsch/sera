7782482684a0f8c345e60166f56c2dbe5062d3b9
stefba <st@rg-s.com>
2020-04-29 12:23:19 +0200

    Refine split tests

--- go/paths/split_test.go
+++ go/paths/split_test.go
@@ -3,18 +3,79 @@ package paths
 import (
 	"github.com/kr/pretty"
 	"testing"
+	"reflect"
 )
 
-var samples = []string{
-	"/index/leben/ueberleben/oeffnungen-33ac2565",
-	"/graph/2020/03/09-36e55605/cache/200310_012140-1280.jpg",
-	"/graph/2020/03/14-3757ceb6/files/200116_235849.mp4",
+type testCase struct {
+	path string
+	result *Path
+}
+
+var cases = []*testCase{
+	&testCase{
+		path: "/index/leben/ueberleben/oeffnungen-33ac2565",
+		result: &Path{
+            Page:    "index",
+            Holds:   []string{"leben", "ueberleben"},
+            Name:    "oeffnungen",
+            Acronym: "33ac2565",
+            Subdir:  "",
+            Subpath: "",
+        },
+	},
+	&testCase{
+		path: "/index/kunst/innen-aussen-35e1fcdd",
+		result: &Path{
+            Page:    "index",
+            Holds:   []string{"kunst"},
+            Name:    "innen-aussen",
+            Acronym: "35e1fcdd",
+            Subdir:  "",
+            Subpath: "",
+        },
+	},
+	&testCase{
+		path: "/index/kunst/form-34a1a15e",
+		result: &Path{
+            Page:    "index",
+            Holds:   []string{"kunst"},
+            Name:    "form",
+            Acronym: "34a1a15e",
+            Subdir:  "",
+            Subpath: "",
+        },
+	},
+	&testCase{
+		path: "/graph/2020/03/09-36e55605/cache/200310_012140-1280.jpg",
+		result: &Path{
+            Page:    "graph",
+            Holds:   []string{"2020", "03"},
+            Name:    "09",
+            Acronym: "36e55605",
+            Subdir:  "cache",
+            Subpath: "200310_012140-1280.jpg",
+        },
+	},
+	&testCase{
+		path: "/graph/2020/03/14-3757ceb6/files/200116_235849.mp4",
+		result: &Path{
+            Page:    "graph",
+            Holds:   []string{"2020", "03"},
+            Name:    "14",
+            Acronym: "3757ceb6",
+            Subdir:  "files",
+            Subpath: "200116_235849.mp4",
+        },
+	},
 }
 
 func TestSplit(t *testing.T) {
-	for _, path := range samples {
-		p := Split(path)
-		t.Logf("\n\n%v\n\n", path)
-		t.Logf("%# v", pretty.Formatter(p))
+	for _, c := range cases {
+		p := Split(c.path)
+		if !reflect.DeepEqual(p, c.result) {
+			t.Errorf("Split failed. Want result:\n\n%# v", pretty.Formatter(c.result))
+		}
+		t.Logf("Sample path:\n\n%v\n\n", c.path)
+		t.Logf("Split produced:\n\n%# v", pretty.Formatter(p))
 	}
 }
