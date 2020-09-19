10ad1e3869fcaca1abe63a50921b8020b4baadae
stefba <st@rg-s.com>
2020-09-19 10:19:09 +0200

    Update complicated service worker

--- static/js/sw.js
+++ static/js/sw.js
@@ -1,5 +1,3 @@
-// Template from here: https://developers.google.com/web/fundamentals/primers/service-workers
-
 var CACHE_NAME = "stferal";
 var urlsToCache = [
   "/",
@@ -19,44 +17,22 @@ const path = url => {
   return url.replace(/^.*\/\/[^\/]+/, "")
 }
 
-let fetchedOption = false;
-
-self.addEventListener("fetch", function(event) {
+// Network falling back to the cache
+self.addEventListener('fetch', function(event) {
   const req = path(event.request.url);
-
-  //  see if option was requested
-  if (req.substr(0, 5) == "/opt/") {
-    fetchedOptions = true;
-  }
-
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-
-        // dont serve index from cache if option was requested
-        if (response && req != "/" && !fetchedOption) {
-          return response;
-        }
-        return fetch(event.request).then(
-          response => {
-
-            if(!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
-
-            // cache new index after option was requested
-            if (req == "/" && fetchedOption) {
-              let responseToCache = response.clone();
-              caches.open(CACHE_NAME)
-                .then(cache => {
-                  cache.put(event.request, responseToCache);
-                  fetchedOption = false;
-                });
-            }
-
-            return response;
-          }
-        );
-      })
-    );
+    fetch(event.request).then(response => {
+      if (req == "/" || req == "manifest.json") {
+        let responseToCache = response.clone();
+        caches.open(CACHE_NAME)
+          .then(cache => {
+            cache.put(event.request, responseToCache);
+          });
+      }
+      return response;
+    }).catch(function() {
+      return caches.match(event.request);
+    })
+  );
 });
+
