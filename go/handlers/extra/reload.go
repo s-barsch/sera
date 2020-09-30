8e0971d4e7aa971c276b63d6fde1a9d8e68602ee
stefba <st@rg-s.com>
2020-09-30 11:50:35 +0200

    Remove constant reload

--- go/handlers/extra/reload.go
+++ go/handlers/extra/reload.go
@@ -4,23 +4,10 @@ import (
 	"log"
 	"net/http"
 	"sacer/go/server"
-
-	//"sacer/go/handlers/index"
-	p "path/filepath"
 )
 
 func AddSlash(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, r.URL.Path+"/", 301)
-}
-
-func ConstantReload(s *server.Server, next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if p.Ext(r.URL.Path) == "" {
-			log.Println(r.URL.Path)
-			s.Reload()
-		}
-		next.ServeHTTP(w, r)
-	})
 }
 
 func Reload(s *server.Server, w http.ResponseWriter, r *http.Request) {
@@ -29,6 +16,4 @@ func Reload(s *server.Server, w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 		http.Error(w, err.Error(), 500)
 	}
-
-	//index.SaveMaps(s)
 }
