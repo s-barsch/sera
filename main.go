5b1cea5a874a7d231dddd5a87d9a50026bb9d5c3
stef barsch <16940454+stefba@users.noreply.github.com>
2025-07-06 10:41:32 +0200

    delete app pkg

--- main.go
+++ main.go
@@ -6,15 +6,38 @@ import (
 	"log"
 	"net/http"
 
-	"g.rg-s.com/sera/go/app"
+	"g.rg-s.com/sera/go/server"
 	"g.rg-s.com/sera/go/server/flags"
+	"g.rg-s.com/sera/go/server/watcher"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	a, err := app.Create(flags.Parse())
+	s, err := initServer()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Fatal(http.ListenAndServe(":8013", a.Router))
+	log.Fatal(http.ListenAndServe(":8013", newRouter(s)))
+}
+
+func initServer() (*server.Server, error) {
+	flags, err := flags.Parse()
+	if err != nil {
+		return nil, err
+	}
+
+	logger := logrus.New()
+
+	s, err := server.New(logger, flags)
+	if err != nil {
+		return nil, err
+	}
+
+	err = watcher.Init(logger, s.Paths, s.Trigger)
+	if err != nil {
+		return nil, err
+	}
+
+	return s, nil
 }
